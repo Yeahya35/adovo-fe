394b9c6b005c7be33f5fc840302d36ceef7b1f38
Yeahya35 <67228196+Yeahya35@users.noreply.github.com>
2025-03-04 02:14:26 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    eslint: { ignoreDuringBuilds: true },
-    output: "export",
-    basePath: "/adovo-fe",
-    assetPrefix: "/adovo-fe",
-    images: { unoptimized: true },
-    trailingSlash: true,
+  output: 'export',
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  images: { unoptimized: true },
+  eslint: { ignoreDuringBuilds: true },
+  trailingSlash: true,
 };
 
 module.exports = nextConfig;
