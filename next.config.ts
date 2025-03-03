e63dc44fbba1b232204216647ea96d28feef41c8
Yeahya35 <67228196+Yeahya35@users.noreply.github.com>
2025-03-04 02:51:08 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@
 const nextConfig = {
   output: 'export',
   basePath: '/adovo-fe',
+  assetPrefix: '/adovo-fe'
   images: { unoptimized: true },
   eslint: { ignoreDuringBuilds: true },
   trailingSlash: true,
