f576a4c51c5240d28e306896ef4fc6ca93b1a38a
yeahya <yahyaeren35@hotmail.com>
2025-03-04 03:31:28 +0300

    fix: image to img

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -35,7 +35,7 @@ export default function Contact() {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex justify-between items-center h-16">
             <Link href="/" className="flex items-center">
-              <Image
+              <img
                 src="/assets/img/adovo-logo.png"
                 alt="ADOVO Logo"
                 width={40}
